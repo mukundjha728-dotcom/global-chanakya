0b5b691f2cb96972c97543f8fc297a88330d4853
Mukund Jha <mukundjha728@gmail.com>
2026-06-30 12:16:21 +0530

    added metadata.verification.google s

--- apps/web/src/app/layout.tsx
+++ apps/web/src/app/layout.tsx
@@ -127,6 +127,7 @@ export default function RootLayout({ children }: Readonly<{ children: React.Reac
   return (
     <html lang="en" suppressHydrationWarning>
       <head>
+        <meta name="google-site-verification" content="VrGd2s0LCdRNrUkVXP2WS7oMOvKxAUD2qZE1Nsepl3A" />
         <link rel="icon" href="/favicon.svg" type="image/svg+xml" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
 
